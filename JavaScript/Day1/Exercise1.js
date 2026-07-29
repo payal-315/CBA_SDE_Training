2f009b0f64f56bec28477eb8d092d57d670f83f0
PAYAL KUMARI <itsmepayal123@gmail.com>
2026-07-29 23:08:57 +0530

    Implement basic arithmetic operations in JavaScript
    This program declares two variables, performs arithmetic operations, and displays the results.

---
+++ JavaScript/Day1/Exercise1.js
@@ -0,0 +1,19 @@
+//Exercise 1: Variables and Arithmetic
+//Problem Statement: Create a JavaScript program that Declares two variables num1 and num2, Stores any two numbers. 
+//Calculates: Sum, Difference, Product, Quotient. Displays all results using console.log().
+
+var num1=20
+var num2=10
+
+var add=num1+num2
+var diff=num1-num2
+var prod=num1*num2
+var quotient=num1/num2
+
+console.log("Number1: "+num1)
+console.log("Number2: "+num2)
+
+console.log("Sum: "+add)
+console.log("Difference: "+diff)
+console.log("Product: "+prod)
+console.log("Quotient: "+quotient)
