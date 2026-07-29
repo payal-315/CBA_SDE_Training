c78af213b0e1301cb139f1b2d6cca4ac01ff2495
PAYAL KUMARI <itsmepayal123@gmail.com>
2026-07-29 23:10:34 +0530

    Add JavaScript program to check even or odd numbers

---
+++ JavaScript/Day1/Exercise2.js
@@ -0,0 +1,11 @@
+//Exercise 2: Even or Odd Number
+//Problem Statement: Write a JavaScript program that: Stores a number in a variable.
+//Checks whether the number is even or odd using an if...else statement. Prints the result.
+
+let a=10
+console.log("Number: "+a)
+if (a%2==0){
+    console.log(a+ " is EVEN")
+}else{
+    console.log(a+ " is ODD")
+}
