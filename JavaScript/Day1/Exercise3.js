86e2c468b3cfa9152e2408cfae3cdc047450a58c
PAYAL KUMARI <itsmepayal123@gmail.com>
2026-07-29 23:12:29 +0530

    Add multiplication table program in Exercise3.js

---
+++ JavaScript/Day1/Exercise3.js
@@ -0,0 +1,8 @@
+//Exercise 3: Multiplication Table
+//Problem Statement: Write a JavaScript program that prints the multiplication table of a given number using a for loop.
+
+let num = 7;
+
+for(let i=1;i<=10;i++){
+    console.log(num+"x"+i+"="+(num*i))
+}
